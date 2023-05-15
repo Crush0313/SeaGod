85a84641531763ce1f13147a4b8af76d0ce20794
crush0313 <sunmoon431@naver.com>
2023-05-15 20:08:12 +0900

    초기 커밋.

---
+++ Assets/Script/MovingCard.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingCard : MonoBehaviour
+{
+    public Transform box1;
+    public Transform box2;
+    public Transform box3;
+
+    public CanvasGroup background;
+
+    public Transform box4;
+    public GameObject window;
+
+    public Image obj;
+
+
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        background.alpha = 0;
+
+        box1.localPosition = new Vector2(-740, -Screen.height - 160);
+        box2.localPosition = new Vector2(0, -Screen.height - 160);
+        box3.localPosition = new Vector2(740, -Screen.height - 160);
+        obj.material.SetFloat("_Size", 0f);
+    }
+
+    public void Opened()
+    {
+        background.LeanAlpha(0.4f, 1f);
+        obj.material.SetFloat("_Size", 10f);
+        box1.LeanMoveLocalY(0, 0.8f).setEaseOutExpo().delay = 0.1f;
+        box2.LeanMoveLocalY(0, 0.8f).setEaseOutExpo().delay = 0.2f;
+        box3.LeanMoveLocalY(0, 0.8f).setEaseOutExpo().delay = 0.3f;
+    }
+
+    public void Closed()
+    {
+        box4.LeanMoveLocalY(box4.localPosition.y * 3, 1f).setEaseOutQuart().delay = 0.2f;
+        background.LeanAlpha(0, 0.3f);
+        TitleWig.on = false;
+        Invoke("windo", 0.9f);
+        obj.material.SetFloat("_Size", 0f);
+
+    }
+
+    public void windo()
+    {
+        CanvasGroup CG = window.GetComponent<CanvasGroup>();
+        window.SetActive(true);
+        CG.alpha = 0f;
+        CG.LeanAlpha(1f, 0.6f);
+    }
+
+    public void QQ()
+    {
+        Closed();
+        box1.LeanMoveLocalY(Screen.height + 160, 0.5f).setEaseOutExpo();
+        box2.LeanMoveLocalY(-Screen.height - 160, 0.5f).setEaseOutExpo();
+        box3.LeanMoveLocalY(-Screen.height - 160, 0.5f).setEaseOutExpo();
+    }
+    public void WW()
+    {
+        Closed();
+        box1.LeanMoveLocalY(-Screen.height - 160, 0.5f).setEaseOutExpo();
+        box2.LeanMoveLocalY(Screen.height + 160, 0.5f).setEaseOutExpo();
+        box3.LeanMoveLocalY(-Screen.height - 160, 0.5f).setEaseOutExpo();
+    }
+    public void EE()
+    {
+        Closed();
+        box1.LeanMoveLocalY(-Screen.height - 160, 0.5f).setEaseOutExpo();
+        box2.LeanMoveLocalY(-Screen.height - 160, 0.5f).setEaseOutExpo();
+        box3.LeanMoveLocalY(Screen.height + 160, 0.5f).setEaseOutExpo();
+    }
+
+}
---
+++ Assets/Script/TitleWig.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleWig : MonoBehaviour
+{
+
+    RectTransform rectTransform;
+    float frame = 0f;
+    float firX;
+    float firY;
+    public float g = 0.1f;
+    public float bae = 10;
+    public static bool on = true;
+
+    public void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        firX = rectTransform.position.x;
+        firY = rectTransform.position.y;
+        StartCoroutine(UpdateCoroutine());
+    }
+
+    private IEnumerator UpdateCoroutine()
+    {
+        while (on == true)
+        {
+            frame += g;
+            rectTransform.position = new Vector2(firX, firY + Mathf.Sin(frame) * bae);
+            yield return new WaitForSeconds(0.015f);
+        }
+    }
+
+}
